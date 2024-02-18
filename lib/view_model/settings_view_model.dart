7a68629a834822278e5fae536e05a50d65dc2faa
Mrvvp <140601476+Mrvvp@users.noreply.github.com>
2024-02-18 06:40:34 +0530

    Update settings_view_model.dart

--- lib/view_model/settings_view_model.dart
+++ lib/view_model/settings_view_model.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class SettingsViewModel extends ChangeNotifier {
-  String _currencySymbol = '\$'; // Default currency symbol
-  double _budget = 0.0; // Default budget
+  String _currencySymbol = '\$'; 
+  double _budget = 0.0; 
 
   String get currencySymbol => _currencySymbol;
   double get budget => _budget;
 
-  // Update currency symbol
+  
   void updateCurrencySymbol(String symbol) {
     _currencySymbol = symbol;
     notifyListeners();
   }
 
-  // Update budget
+  
   void updateBudget(double newBudget) {
     _budget = newBudget;
     notifyListeners();
